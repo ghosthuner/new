18bc487b114da5043176519e2a97af0e52b669cc
Admin <Admin@DESKTOP-QI6H2EA>
2025-03-21 16:09:18 +0300

    try  commit

--- newPackage/newClass.java
+++ newPackage/newClass.java
@@ -11,5 +11,6 @@ public class newClass {
     
    public static void main(String[] args) {
         System.out.println("Try programiz.pro");
+        System.out.println("this si me trying ");
     }
 }
